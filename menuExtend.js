e4e500bfd0c54859bf4a3196c0ac3c2caa8d3a4d
seewilds <wildmanc@hotmail.com>
2022-01-15 23:53:20 -0500

    menu complete

--- menuExtend.js
+++ menuExtend.js
@@ -1,17 +1,39 @@
 function menuExtend(){
     const mainNav = document.querySelector('nav');
-    mainNav.style.display='inline-block';
-    console.log(mainNav);
-
     const menuNav = document.querySelector('#menu');
-    menuNav.style.display='none';
+    var mq = window.matchMedia( "(max-width: 700px)" );
+    if(mq.matches){
+        mainNav.style.display='inline-block';
+        menuNav.style.display='none';
+    }else{
+        mainNav.style.display='inline-block';
+        menuNav.style.display='none';
+    }
+    
 }
 
 function menuCollapse(){
     const mainNav = document.querySelector('nav');
-    mainNav.style.display='none';
-    console.log(mainNav);
-
     const menuNav = document.querySelector('#menu');
-    menuNav.style.display='inline-block';
+    var mq = window.matchMedia( "(max-width: 700px)" );
+    if(mq.matches){
+        mainNav.style.display='none';
+        menuNav.style.display='inline-block';
+    }
+    
 }
+
+function resizeTest(){
+    var mq = window.matchMedia( "(max-width: 700px)" );
+    const mainNav = document.querySelector('nav');
+    const menuNav = document.querySelector('#menu');
+    if(mq.matches){
+        mainNav.style.display='none';
+        menuNav.style.display='inline-block';
+    }else{
+        mainNav.style.display='inline-block';
+        menuNav.style.display='none';
+    }
+}
+
+window.onresize = resizeTest;
