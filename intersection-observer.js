5dbcc8f018d14cb79607795764e9838aad029333
seewilds <wildmanc@hotmail.com>
2022-01-15 19:21:25 -0500

    new sections, adding stuff

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,9 +1,8 @@
-const sectionOne = document.querySelector('.sectionOne');
 const faders = document.querySelectorAll('.fade-in');
 
 const options = {
     root: null, //default is viewport
-    threshold: 0, // bewteen 0 and 1 - if at 1 100% if item has to be on the page
+    threshold: .25, // bewteen 0 and 1 - if at 1 100% if item has to be on the page
     rootMargin: "-50px" //pixels percentage
 };
 
@@ -16,13 +15,9 @@ const observer = new IntersectionObserver((entries, options)=>{
             entry.target.classList.remove('disappear');
             entry.target.classList.add('appear');
         }
-        //console.log(entry);
-        //observer.unobserve(entry.target); // stop observing once seen, even after scroll up
     });
 }, options);
 
 faders.forEach(fader=>{
     observer.observe(fader);
 })
-
-//observer.observe(sections);
